a8ac3a30b2067905f437d2234ce934c888435bdd
Jalyn-is <jalynmessado11@gmail.com>
2025-08-26 08:17:30 -0500

    Created the layout for the getHumanChoice function

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //console.log('hello wrld'); //The console works muhuhaha!
 
-function getComputerChoice (min, max){
+function getComputerChoice (min, max){ 
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); 
 }
-console.log(getComputerChoice(1,4));
+function getHumanChoice (){
+    
+}
