8c20b1f8a505e0040809e14656e9241663cf101a
Jalyn-is <jalynmessado11@gmail.com>
2025-08-26 19:47:35 -0500

    Created the array that takes the random value from math.random and chnages it to either rock, paper or scissors

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 //console.log('hello wrld'); //The console works muhuhaha!
 
 function getComputerChoice (min, max){ 
+    const options = ['rock' , 'paper' , 'scissors']
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); 
+    return options[Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)];
 }
+console.log(getComputerChoice(0,3));
+console.log(getComputerChoice(0,3));
+console.log(getComputerChoice(0,3));
 function getHumanChoice (){
     
 }
