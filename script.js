60b5068c2fded93a1b6e64fd1be5ec89c4accebf
Jalyn-is <jalynmessado11@gmail.com>
2025-08-27 00:00:14 -0500

    Created 2 global variables to keep track  of human and computer score.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //console.log('hello wrld'); //The console works muhuhaha!
 
+const humanScore = 0;
+const computerScore =0;
+
 function getComputerChoice (min, max){ 
     const options = ['rock' , 'paper' , 'scissors']
     const minCeiled = Math.ceil(min);
@@ -7,6 +10,12 @@ function getComputerChoice (min, max){
     return options[Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)];
 }
 function getHumanChoice (){
-   const choice = prompt('rock paper scissors?')
-   return choice; 
+    const humanChoice = prompt('rock paper scissors?').toLowerCase().split(' ').join('')
+    if (humanChoice === 'rock' || humanChoice === 'paper' || humanChoice === 'scissors') {
+        console.log()
+    } else {
+        prompt ('Invalid input')
+    }
+    return humanChoice;
 }
+
