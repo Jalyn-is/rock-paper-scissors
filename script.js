ffc6a77e04f1796780b3a0bb37d25eaa6f693d07
Jalyn-is <jalynmessado11@gmail.com>
2025-09-01 23:01:50 -0500

    The game is now complete, the last function simply ran playRound 5 times and printed the result for a winner or a tie.

--- script.js
+++ script.js
@@ -29,12 +29,32 @@ function playRound(){
     (humanSelection === "scissors" && computerSelection === "paper")
   ) {
     humanScore++;
-    return `You win! ${humanSelection} beats ${computerSelection}. Current socre: You ${humanScore} - Computer ${computerScore}`;
+    return `You win! ${humanSelection} beats ${computerSelection}. Current socre: You: ${humanScore} - Computer: ${computerScore}`;
   } else {
     computerScore++;
-    return `You lose! ${computerSelection} beats ${humanSelection}. Current score: You ${humanScore} - Computer ${computerScore}`;
+    return `You lose! ${computerSelection} beats ${humanSelection}. Current score: You: ${humanScore} - Computer: ${computerScore}`;
   }
 } 
-console.log(playRound())
+function playGame() {
+  humanScore = 0;
+  computerScore = 0;
+
+  console.log(playRound());
+  console.log(playRound());
+  console.log(playRound());
+  console.log(playRound());
+  console.log(playRound());
+
+  if (humanScore > computerScore) {
+  return (`You win the game! Final score: You ${humanScore} - Computer ${computerScore}`);
+} else if (computerScore > humanScore) {
+  return (`Computer wins the game! Final score: Computer ${computerScore} - You ${humanScore}`);
+} else {
+  return (`It's a tie! Final score: You ${humanScore} - Computer ${computerScore}`);
+}
+}
+console.log(playGame())
+
+
 
  
